4f623cf07f296db5311c2d5f5caeefab9d720c7a
beattycharles <113803621+beattycharles@users.noreply.github.com>
2023-03-19 16:59:54 -0400

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="Nav">
+      <ul className="Bar">
+        <li>Home</li>
+        <li>Projects</li>
+        <li>Contact Me</li>
+      </ul>
+    </div>
+  );
+}
